43e4a08c7b33bab644b55e1965fb142a6b944664
phuct <phubierd@gmail.com>
2025-12-14 14:53:25 +0700

    Initial Commit

---
+++ ToDoApp_Youtube/ToDoApp_YoutubeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp_YoutubeApp.swift
+//  ToDoApp_Youtube
+//
+//  Created by Phú Chiêm on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp_YoutubeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
